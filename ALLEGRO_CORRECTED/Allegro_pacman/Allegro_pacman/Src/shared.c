a34dc30da3c6d89fe8375782a9148640eca38710
edbert <s112006207@m112.nthu.edu.tw>
2025-08-26 14:05:41 +0800

    PUSH ALL

---
+++ ALLEGRO_CORRECTED/Allegro_pacman/Allegro_pacman/Src/shared.c
@@ -0,0 +1,48 @@
+// [shared.c]
+// you should define the shared variable declared in the header here.
+
+#include "shared.h"
+#include "utility.h"
+#include "game.h"
+// #include "scene_menu.h"
+
+ALLEGRO_FONT* font_pirulen_32;
+ALLEGRO_FONT* font_pirulen_24;
+ALLEGRO_SAMPLE* themeMusic = NULL;
+ALLEGRO_SAMPLE* PACMAN_MOVESOUND = NULL;
+ALLEGRO_SAMPLE* PACMAN_DEATH_SOUND = NULL;
+ALLEGRO_SAMPLE* PACMAN_EAT_GHOST = NULL;
+ALLEGRO_FONT* menuFont = NULL;
+ALLEGRO_FONT* LFont = NULL;
+ALLEGRO_FONT* WFont = NULL;
+int fontSize1 = 30;
+int fontSize2 = 80;
+int fontSize3 = 50;
+float music_volume = 0.5;
+float effect_volume = 0.5;
+bool gameDone = false;
+
+/*
+	A way to accelerate is load assets once.
+	And delete them at the end.
+	This method does provide you better management of memory.
+	
+*/
+void shared_init(void) {
+	
+	menuFont = load_font("Assets/Minecraft.ttf", fontSize1);
+	LFont = load_font("Assets/Minecraft.ttf", fontSize2);
+	WFont = load_font("Assets/Minecraft.ttf", fontSize3);
+	themeMusic = load_audio("Assets/Music/original_theme.ogg");
+	PACMAN_MOVESOUND = load_audio("Assets/Music/pacman-chomp.ogg");
+	PACMAN_DEATH_SOUND = load_audio("Assets/Music/pacman_death.ogg");
+	PACMAN_EAT_GHOST = load_audio("Assets/Music/pacman_eatghost.ogg");
+}
+
+void shared_destroy(void) {
+
+	al_destroy_font(menuFont);
+	al_destroy_sample(themeMusic);
+	al_destroy_sample(PACMAN_MOVESOUND);
+	al_destroy_sample(PACMAN_DEATH_SOUND);
+}
